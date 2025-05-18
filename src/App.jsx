6a90a0867a95caf12f58f228897b1f3178ee9336
gitdeepaks <deepaksankhyan92@outlook.com>
2025-05-18 15:46:08 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import './App.css';
-
 function App() {
-  return <div className="">Hello</div>;
+  return (
+    <>
+      <h1 className="text-3xl underline font-bold text-center items-center justify-center flex h-screen">
+        Hello
+      </h1>
+    </>
+  );
 }
 
 export default App;
